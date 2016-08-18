0e4ee5beac6147f683fafec69c9ca12cd2cfa64a
Taehong Kim <peppy0510@hotmail.com>
2016-08-18 19:42:29 +0900

    update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except ImportError:
 
 setup(
     name='dtruct',
-    version='0.1.4',
+    version='0.1.5',
     packages=['dtruct'],
     author='Taehong Kim',
     author_email='peppy0510@hotmail.com',
